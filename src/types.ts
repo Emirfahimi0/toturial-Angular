92b030bcee37f5f8dfed48f65f489dd57bf872ff
Emirfahimi0 <emirfahimi0@gmail.com>
2024-03-22 14:58:05 +0800

    update types

---
+++ src/types.ts
@@ -0,0 +1,53 @@
+import { HttpHeaders, HttpContext, HttpParams } from '@angular/common/http';
+
+export interface IOptions {
+  headers?:
+    | HttpHeaders
+    | {
+        [header: string]: string | string[];
+      };
+  observe?: 'body';
+  context?: HttpContext;
+  params?:
+    | HttpParams
+    | {
+        [param: string]:
+          | string
+          | number
+          | boolean
+          | ReadonlyArray<string | number | boolean>;
+      };
+  reportProgress?: boolean;
+  responseType?: 'json';
+  withCredentials?: boolean;
+  transferCache?:
+    | {
+        includeHeaders?: string[];
+      }
+    | boolean;
+}
+
+export interface IProduct {
+  image: string;
+  name: string;
+  price: string;
+  rating: number;
+}
+
+export interface IProducts {
+  items: IProduct[];
+  page: number;
+  perPage: number;
+  total: number;
+  totalPages: number;
+}
+
+export interface IPaginationParams {
+  [param: string]:
+    | string
+    | number
+    | boolean
+    | ReadonlyArray<string | number | boolean>;
+  page: number;
+  perPage: number;
+}
