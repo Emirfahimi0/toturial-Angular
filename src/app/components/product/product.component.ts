c395ca75d120efdd49e8c3b0ccbc70eb92fdc897
Emirfahimi0 <emirfahimi0@gmail.com>
2024-03-22 14:59:09 +0800

    added product component

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { IProduct } from '../../../types';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [RatingModule, FormsModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss',
+})
+export class ProductComponent {
+  @Input() product!: IProduct;
+}
