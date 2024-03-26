e06b28b15e8c2ef59b851c6d9202c04ffe412238
Emirfahimi0 <emirfahimi0@gmail.com>
2024-03-26 08:34:08 +0800

    update components

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -11,10 +11,19 @@ import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { ConfirmationService } from 'primeng/api';
+import { PricePipe } from '../../pipes/price.pipe';
+import { TruncateNamePipe } from '../../pipes/truncate-name.pipe';
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule, ButtonModule, ConfirmPopupModule],
+  imports: [
+    ButtonModule,
+    ConfirmPopupModule,
+    FormsModule,
+    PricePipe,
+    RatingModule,
+    TruncateNamePipe,
+  ],
   providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
