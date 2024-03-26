2cdf745ccab9e8917c30daaa5f81489b19682eef
Emirfahimi0 <emirfahimi0@gmail.com>
2024-03-26 08:34:19 +0800

    add modules

---
+++ src/app/modules/about-us/about-us.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AboutUsRoutingModule } from './about-us-routing.module';
+
+@NgModule({
+  declarations: [AboutUsComponent],
+  imports: [CommonModule, AboutUsRoutingModule],
+  exports: [],
+  providers: [],
+})
+export class AboutUsModule {}
